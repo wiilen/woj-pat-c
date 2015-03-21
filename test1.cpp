46ae151cb93688354ecf3844949fc5db17b396df
wiilen <wiilen.png@gmail.com>
2015-03-21 15:38:37 +0800

    Revert "c file"
    This reverts commit a4f6cdf99e4fd1a61a27a9652c49c340128b1cb0.

--- test1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<cstdio>
-using namespace std;
-
-int main(){
-	char a[20];
-	gets(a);
-	printf("%s",a);
-}
--- 分段函数计算.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-
-int main(){
-	double x,y,c;
-	scanf("%lf %lf",&x,&c);
-	y=fmod(x,c);
-	printf("%lf",y); 
-}
